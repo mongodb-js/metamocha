25d252a7305aeed1a79780362d62a516c9d53155
Marcus Todd <malexander.todd@gmail.com>
2017-06-19 12:49:52 -0400

    init

---
+++ test/runner.js
@@ -0,0 +1,29 @@
+var Mocha = require('mocha'),
+    Suite = Mocha.Suite,
+    Runner = Mocha.Runner,
+    Test = Mocha.Test,
+    fs = require('fs'),
+    path = require('path'),
+    metadata_ui = require('./metadata_ui');
+
+
+var argv = require('optimist')
+    .usage('Usage: $0 -t [target]')
+    .demand(['t'])
+    .argv;
+
+// Instantiate new Mocha instance
+var mocha = new Mocha({ui: 'metadata_ui'});
+
+// Add the test file
+mocha.addFile(argv.t);
+
+// Run the tests
+mocha.run(function(failures) {
+  process.on('exit', function() {
+    process.exit(failures);
+  })
+
+  process.exit();
+})
+
