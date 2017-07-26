b7ee1ee004ae7d1abf80d19bf4bb3eaa1216478a
Marcus Todd <malexander.todd@gmail.com>
2017-07-26 14:49:43 -0400

    style(*): small capitalization and spacing fixes
    NODE-1082

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 
-exports.metamocha = require('./lib/metamocha');
-exports.metadata_ui = require('./lib/metadata_ui');
+exports.Metamocha = require('./lib/metamocha');
+exports.MetadataUi = require('./lib/metadata_ui');
